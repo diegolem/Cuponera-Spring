c5eacfad3bf578c07ce88a092d1a43fa93640788
DiegoLem <ciegolem7@gmail.com>
2018-11-04 00:18:12 -0600

    Lista de cupones
    - Falta paginacion y compra

--- src/main/java/sv/edu/udb/www/cuponera/repositories/SalesRepository.java
+++ src/main/java/sv/edu/udb/www/cuponera/repositories/SalesRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import sv.edu.udb.www.cuponera.entities.Promotions;
 import sv.edu.udb.www.cuponera.entities.Sales;
 import sv.edu.udb.www.cuponera.entities.Users;
 
@@ -17,6 +18,9 @@ public interface SalesRepository extends JpaRepository<Sales,String> {
 	public abstract Sales findByCouponCode(String couponCode);
 	
 	public abstract List<Sales> findByClient(Users user);
+	
+	@Query("SELECT p FROM Promotions p WHERE p.state.id = 2 AND p.initDate <= CURRENT_DATE AND p.endDate >= CURRENT_DATE AND p.limitDate >= p.endDate ORDER BY p.limitDate")
+	public List<Promotions> listPromotionsAvailable();
 	
 	@Query("select count(s) > 0 from Sales s where s.couponCode = :couponCode")
 	public boolean existCupon(@Param("couponCode") String couponCode);
